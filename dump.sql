70bfea6c546fa3dd5e4bf8742ad359b5a9e1bc92
Uenderss <meajudabarto@gmail.com>
2022-08-17 00:03:04 -0300

    create cakes

--- dump.sql
+++ dump.sql
@@ -1,22 +1,29 @@
-CREATE TABLE cakes (
+CREATE DATABASE "la_Boleira"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE TABLE  IF NOT EXISTS cakes (
 id serial NOT NULL PRIMARY KEY,
-`name`  VARCHAR(100) NOT NULL,
-`price` NUMERIC(5,2) NOT NULL,
-`image` VARCHAR(255) NOT NULL,
-`description` TEXT NOT NULL,
+"name"  VARCHAR(100) NOT NULL,
+"price" NUMERIC(5,2) NOT NULL,
+"image" VARCHAR(255) NOT NULL,
+"description" TEXT NOT NULL,
 );
 
-CREATE TABLE clients (
+CREATE TABLE  IF NOT EXISTS clients (
     id serial NOT NULL PRIMARY KEY,
-    `name`  VARCHAR(100) NOT NULL,
-    `address` VARCHAR(255) NOT NULL,
-    `phone` VARCHAR(15) NOT NULL
+    "name"  VARCHAR(100) NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(15) NOT NULL
 );
-   CREATE TABLE orders (
+   CREATE TABLE  IF NOT EXISTS orders (
     id serial NOT NULL PRIMARY KEY,
-     `clientId` INTEGER NOT NULL,
-     `cakeId` INTEGER NOT NULL,
-     `quantity` INTEGER NOT NULL,
-     `createdAt` timestamp NOT NULL DEFAULT NOW(),
-     `totalPrice` NUMERIC (5,2)
+     "clientId" INTEGER NOT NULL,
+     "cakeId" INTEGER NOT NULL,
+     "quantity" INTEGER NOT NULL,
+     "createdAt" timestamp NOT NULL DEFAULT NOW(),
+     "totalPrice" NUMERIC (5,2)
    );

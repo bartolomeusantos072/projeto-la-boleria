9edc52fc365a5b3c881717e83655b56ff6e10cbd
Uenderss <meajudabarto@gmail.com>
2022-08-18 19:52:00 -0300

    correcao em get orders

--- src/middlewares/orderValidate.js
+++ src/middlewares/orderValidate.js
@@ -1,25 +1,25 @@
 import clientsRepository from "../repositories/clientsRepository.js";
 import cakeRepository from "../repositories/cakeRepository.js";
+import orderRepository from "../repositories/orderRepository.js";
 
 export async function orderValidate(req, res, next) {
-  const { clientId, cakeId, quantity, totalPrice } = req.body;
+  const { clientId, cakeId, quantity } = req.body;
   console.log(req.body)
   try {
     const msg =[];
 
     const verifyClient = await clientsRepository.checkClientId(clientId);
-    if (verifyClient.rowCount < 1) {
-      msg.push("client not exist");
+    if (verifyClient.rowCount ===0) {
+      return res.sendStatus(400);
     }
 
     const verifyCake = await cakeRepository.checkCakeId(cakeId);
     if(verifyCake.rowCount < 1){
-      msg.push(" \n cake not exist");
+      return res.sendStatus(404);
     }
-    
-
-    res.status(400).send(msg);
-
+    if(quantity > 4 || quantity <1){
+      return res.sendStatus(400)
+    }
     next();
   } catch (e) {
     console.log(e);
--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -10,30 +10,48 @@ async function insertOrder(order) {
     return database.query('INSERT INTO orders ("clientId", "cakeId", quantity, "totalPrice") VALUES ($1, $2, $3,$4)', [clientId, cakeId, quantity, totalPrice]);
 }
 
-async function getOrderByBody(){
-    return database.query(`SELECT clients.*, cakes.*, orders.id, orders."createdAt", orders.quantity, orders."totalPrice"  FROM orders JOIN clients ON clients.id=orders."clientId" JOIN cakes ON cakes.id=orders."cakeId"`)
+
+
+async function getOrderByBody() {
+    return database.query(`SELECT 
+    clients.id AS "clientId", clients.name AS "nameClient", clients.address, clients.phone, 
+    cakes.id AS "cakeId", cakes.name AS "nameCake", cakes.price, cakes.description, cakes.image, 
+    orders.id AS "orderId", orders."createdAt", orders.quantity, orders."totalPrice"
+    FROM orders JOIN clients ON clients.id=orders."clientId" JOIN cakes ON cakes.id=orders."cakeId"`)
 }
 
-async function getOrderByQuery(date){
-    return database.query(`select clients.*, cakes.*, orders.id, orders."createdAt", orders.quantity, orders."totalPrice" 
+async function getOrderByQuery(date) {
+    return database.query(`SELECT
+    clients.id AS "clientId", clients.name AS "nameClient", clients.address, clients.phone,
+    cakes.id AS "cakeId", cakes.name AS "nameCake", cakes.price, cakes.description, cakes.image, 
+    orders.id AS "orderId", orders."createdAt", orders.quantity, orders."totalPrice" 
     from orders
     JOIN clients ON clients.id=orders."clientId"
     JOIN cakes ON cakes.id=orders."cakeId"
-    WHERE DATE(orders."createdAt")=$1`,[date])
+    WHERE DATE(orders."createdAt")=$1`, [date])
 }
- async function getOrderById(id){
+async function getOrderById(id) {
     return database.query(`select clients.*, cakes.*, orders.id, orders."createdAt", orders.quantity, orders."totalPrice" 
     from orders
     JOIN clients ON clients.id=orders."clientId"
     JOIN cakes ON cakes.id=orders."cakeId"
-    WHERE orders.id=$1`,[id])
- }
- async function checkOrderForClient(id){
+    WHERE orders.id=$1`, [id])
+}
+
+async function checkOrderForClient(id) {
     return database.query(`select clients.*, cakes.*, orders.id, orders."createdAt", orders.quantity, orders."totalPrice" 
-    from orders
-    JOIN clients ON clients.id=orders."clientId" AND orders."clientId"=$1
-    JOIN cakes ON cakes.id=orders."cakeId"`,[id])
- }
+     from orders
+     JOIN clients ON clients.id=orders."clientId" AND orders."clientId"=$1
+     JOIN cakes ON cakes.id=orders."cakeId"`, [id])
+}
+
+async function checkOrderCakeId(id) {
+    return database.query(`
+       SELECT id
+       FROM orders 
+       WHERE "cakeId" = $1
+     `, [id]);
+}
 
 const cakeRepository = {
     checkOrder,
@@ -41,7 +59,8 @@ const cakeRepository = {
     getOrderByBody,
     getOrderByQuery,
     getOrderById,
-    checkOrderForClient
+    checkOrderForClient,
+    checkOrderCakeId
 };
 
 export default cakeRepository;
